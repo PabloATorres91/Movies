ec185a6af8fb72af437a0668518d0a3dfe54902d
Pablo Torres <p.torres0991@gmail.com>
2022-02-04 21:12:29 -0300

    Version inicial de Movies.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from "./App.jsx";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const contenido = 
+(
+  <div>
+    <h1>Titulo</h1>
+    <div>Hola desde React</div>
+  </div>
 );
+/** Opcion mas rustica de hacer que pueda declarar un Componente para renderizar y que se transforme su contenido (en este caso un elemento h1 y un div)
+ * function Componente ()
+ * {
+ *    return(
+ *      <div> 
+ *        <h1>Titulo</h1>
+ *        <div>Hola desde React!</div>
+ *      </div>
+ *    );
+ *  }
+ *  
+ *  ReactDOM.render(
+ *    <Componente/>,
+ *    document.getElementById('root')
+ *    );
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/** Opcion trabajada con desestructuracion y enviando parametros desde la funcion ReactDOM.render. Se utiliza el objeto que se genera al enviar atributos para la desestructuracion.
+ * Se envia titulo y contenido pero se recibe desestructurandolo en titulo y contenido como properties.
+ * 
+ * function Componente( {titulo, contenido} )
+ * {
+ *   return(
+ *       <div> 
+ *         <h1>{titulo}</h1>
+ *         <div>{contenido}</div>
+ *       </div>
+ *   );
+ * } 
+ * 
+ * ReactDOM.render(
+ *   <Componente titulo="Titulo ejemplo 2" contenido="Hola desde React ejemplo 2"/>,
+ *     document.getElementById('root')
+ * );
+ */
+
+function Componente({ titulo, children }) {
+  return (
+    <div>
+      <h1>{titulo}</h1>
+      <div>{children}</div>
+    </div>
+  );
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
